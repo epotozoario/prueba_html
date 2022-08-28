3ccef780917d45eda23085a1f49dd07445e7e2db
luis <leonluiscar@hotmail.com>
2022-08-27 22:40:05 -0500

    modificado

---
+++ javascript/primerjs.js
@@ -0,0 +1,7 @@
+function funcion1(){
+    //let a=parseInt(prompt("digite el numero 1"));
+    //let b=12;
+    //let c=a+b;
+    //document.getElementById("prueba").innerHTML="la suma es igual a "+c;
+    document.getElementById("prueba").innerHTML="cambio de parrafo "
+}
--- primerjs.js
+++
@@ -1,6 +0,0 @@
-function funcion1(){
-    let a=parseInt(prompt("digite el numero 1"));
-    let b=12;
-    let c=a+b;
-    document.getElementById("prueba").innerHTML="la suma es igual a "+c;
-}
